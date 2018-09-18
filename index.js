63b0b3c0d3af0e4f143fd0e16dda0d15a89815f7
Peter Sefton <peter.sefton@uts.edu.au>
2018-09-18 12:52:57 +1000

    Pairtree stuff working now - still need to test on single-file output

--- index.js
+++
@@ -1,9 +0,0 @@
-
-
-module.exports = {
-	Collection: require('./lib/collection.js'),
-	Index: require('./lib/index_html.js'),
-	Datacite: require('./lib/datacite.js'),
-	Bag: require('./lib/bag.js')
-}
-
