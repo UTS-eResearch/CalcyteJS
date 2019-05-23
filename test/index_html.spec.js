9b1bb448cf7fd6a0e9d88950553e5dc25af72292
Mike Lynch <mike@mikelynch.org>
2019-05-23 14:59:31 +1000

    Added async tests, which aren't passing

--- test/index_html.spec.js
+++ test/index_html.spec.js
@@ -28,6 +28,7 @@ const expect = chai.expect;
 
 const DEFAULT_TEST_DIR = "test_output";
 const OUTPUT_DIR = path.join(DEFAULT_TEST_DIR, "output");
+const OUTPUT_SYNC_DIR = path.join(DEFAULT_TEST_DIR, "output_sync");
 const SAMPLE_CATALOG = "test_data/sample_CATALOG.json";
 const FIXTURE = "test_data/fixtures/sample_html";
 const TIMESTAMP = '2019-05-16T03:09:09.518Z';
@@ -35,7 +36,7 @@ const text_citation_1 =
   "Peter Sefton (2017) Sample dataset for DataCrate v0.2. University of Technology Sydney. Datacrate. http://dx.doi.org/10.5281/zenodo.1009240";
 const assert = require("assert");
 
-describe("Test sync creation of multi-file html", function() {
+describe("Sync creation of multi-file html", function() {
 
   before(async function() {
     await fs.remove(DEFAULT_TEST_DIR);
@@ -46,7 +47,7 @@ describe("Test sync creation of multi-file html", function() {
 
 
 
-  it("Can generate multi-page CATALOG.html files with the right paths", function() {
+  it("Can synchronously create multi-page CATALOG.html file structure", function() {
     var index_maker = new Index();
 
     index_maker.init({
@@ -75,7 +76,7 @@ describe("Test sync creation of multi-file html", function() {
 });
 
 
-it.skip("Can generate multi-page CATALOG.html files with the right content", function() {
+it.skip("Can synchronously create multi-page CATALOG.html file contents", function() {
     var index_maker = new Index();
 
     index_maker.init({
@@ -108,33 +109,78 @@ it.skip("Can generate multi-page CATALOG.html files with the right content", fun
 });
 
 
+// Notes on the async tests: 
+// create one CATALOG_files with the sync method, create one from the same JSON with the async
+// methods, and then use the 'deep.files.that.satisfy' technique above to ensure that they are the
+// same
 
 
-// describe("Test async creation of CATALOG.html", function() {
-
-//   before(async function() {
-//     await fs.remove(DEFAULT_TEST_DIR);
-//     await fs.ensureDir(DEFAULT_TEST_DIR);
-//   })
 
 
-//   it("Can generate a single-page CATALOG.html", async function() {
-//     var index_maker = new Index();
+describe("Async creation of multi-file html", function() {
 
-//     var template_ejs = await fs.readFile(defaults.catalog_template);
-//     var catalog_json = await fs.readJson(SAMPLE_CATALOG);
-
-//     index_maker.init_pure({
-//       catalog_json: catalog_json,
-//       template: template_ejs,
-//       multiple_files: false
-//     });
-
-//     assert.equal(
-//       index_maker.get_href("http://dx.doi.org/10.5281/zenodo.1009240"),
-//       "#http://dx.doi.org/10.5281/zenodo.1009240"
-//     );
+  before(async function() {
+    await fs.remove(DEFAULT_TEST_DIR);
+    await fs.ensureDir(DEFAULT_TEST_DIR);
+  })
 
 
-//   });
-// });
+  it("Can asynchronously create multi-page CATALOG.html file structure", async function() {
+    var index = new Index();
+    var index_sync = new Index();
+
+    var template_ejs = await fs.readFile(defaults.catalog_template);
+    var catalog_json = await fs.readJson(SAMPLE_CATALOG);
+
+    index.init_pure({
+      catalog_json: catalog_json,
+      template: template_ejs,
+      multiple_files: false
+    });
+
+    const pages = index.make_index_pages(text_citation_1, "zip_path");
+
+    for( var i in pages ) {
+      const p = pages[i];
+      const page_html = index.template_html(p.node, p.html);
+      const out_path = path.join(this.out_dir, index.get_html_path(p.node['@id']));
+      await fs.ensureDir(out_path);
+      await fs.writeFile(path.join(out_path, index.html_file_name), page_html);
+    }
+
+    // make a sync one to compare it with
+
+    index_sync.init({
+      catalog_json: path.join("..", SAMPLE_CATALOG),
+      out_dir: OUTPUT_SYNC_DIR,
+      multiple_files: true,
+      force_timestamp: TIMESTAMP,
+      force_json_timestamp: TIMESTAMP
+    });
+
+    // this is sync
+
+    index_sync.make_index_html(
+      text_citation_1, "zip_path"
+    );
+
+
+    expect(OUTPUT_DIR).to.be.a.directory("is a dir").with.deep.files.that.satisfy((files) => {
+      return files.every((file) => {
+        const fixture_file = path.join(OUTPUT_SYNC_DIR, file);
+        const output_file = path.join(OUTPUT_DIR, file);
+        if ( file !== 'CATALOG.html' ) {
+          expect(output_file).to.be.a.file(`file ${output_file}`).and.equal(fixture_file, `${output_file} content matches`);
+        }
+        return true;
+      })
+    })
+
+
+
+
+
+
+
+  });
+});
