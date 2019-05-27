ef50486c135d25f15d95c30ccd6bd9b494415962
Mike Lynch <mike@mikelynch.org>
2019-05-27 15:10:33 +1000

    Reinstating the index.js, which I accidentally deleted when merging the async-friendly branch

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+module.exports = {
+	Collection: require('./lib/collection.js'),
+	Index: require('./lib/index_html.js'),
+	Datacite: require('./lib/datacite.js'),
+	Bag: require('./lib/bag.js'),
+	jsonld: require('./lib/jsonldhelper.js'),
+	defaults: require('./lib/defaults.js')
+}
+
